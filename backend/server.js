588fee085ff0c6a22e862341bca875af1830d4f1
jnepmark <123545579+jnepmark@users.noreply.github.com>
2024-04-18 20:52:16 +0200

    18042024

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const multer = require('multer')
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    return cb(null, "./public/Images")
+    cb(null, "./public/Images");
   },
   filename: function (req, file, cb) {
-    return cb(null, `${Date.now()}_${file.originalname}`)
+    cb(null, `${Date.now()}_${file.originalname}`);
   }
-})
+});
 
-const upload = multer({storage})
+const upload = multer({ storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  console.log(req.body)
-  console.log(req.file)
-})
+app.post('/upload', upload.array('cat_pics', 10), (req,res) => {
+   console.log(req.body);
+   console.log(req.files);
+ 
+});
 
 app.listen(4000, () => {
-  console.log("Server is running")
-})
+    console.log("Server is running");
+});
--- multer/src/App.jsx
+++ multer/src/App.jsx
@@ -1,21 +1,49 @@
-import React, { useState } from "react"
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [file, setFile] = useState()
+  const [file, setFile] = useState();
+  const [imageUrl, setImageUrl] = useState();
+
   const upload = () => {
-    const formData = new FormData()
-    formData.append('file', file)
-    axios.post('http://localhost:4000/upload',formData )
-    .then( res => {})
-    .catch(er => console.log(er))
+    if (!file || file.length === 0) {
+      alert("No file to upload");
+      return;
+    }
+
+    const formData = new FormData();
+    Array.from(file).forEach((file) => {
+      formData.append('cat_pics', file);
+    });
+
+    axios.post("http://localhost:4000/upload", formData)
+      .then(res => {
+        if (res.data.status === 'success') { 
+          setImageUrl(res.data.imageUrl);
+          alert("Uploaded successfully"); // Alert on successful upload
+        } else { 
+          console.log("Upload failed");
+          alert("Upload failed"); // Alert on failed upload
+        }
+      })
+      .catch(err => console.log(err));
   }
-   return (
-    <div>
-      <input type="file" onChange={(e) => setFile(e.target.files[0])}/>
-      <button type="button" onClick={upload}>Upload</button>
-    </div>
-  )
+
+  return (
+     <div>
+       <form action="/upload" method="post" encType="multipart/form-data">
+         <div>
+           <label>Select your cat pictures:</label>
+           <input multiple="multiple" name="cat_pics" type="file" onChange={(e) => setFile(e.target.files)} />
+           {/* Call `upload` function on button click */}
+           <button type="button" onClick={upload}>Upload Cat Pics</button>
+         </div>
+       </form>
+
+       {imageUrl && 
+        	<img src={`http://localhost:4000/${imageUrl}`} alt="uploaded"/>} {}
+     </div> 
+   );
 }
 
 export default App;
