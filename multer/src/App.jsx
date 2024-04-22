5ca83df5797fcb932056c4962e21bb52f0853313
jnepmark <123545579+jnepmark@users.noreply.github.com>
2024-04-22 20:49:32 +0200

    DELETE

--- multer/src/App.jsx
+++ multer/src/App.jsx
@@ -1,14 +1,14 @@
-import{ useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import ImageCard from './ImageCard';
 import './App.css';
 
 function App() {
-  const [file, setFile] = useState();
+  const [file, setFile] = useState([]);
   const [imageUrls, setImageUrls] = useState([]);
 
   const upload = () => {
-    if (!file || file.length === 0) {
+    if (file.length === 0) {
       alert("No file to upload");
       return;
     }
@@ -31,8 +31,7 @@ function App() {
       .catch(err => console.log(err));
   }
 
-   const handleDelete = (index) => {
-     // Create a copy of the imageUrls array without the deleted item
+  const handleDelete = (index) => { 
      let updatedImages = [...imageUrls];
      updatedImages.splice(index, 1);
      setImageUrls(updatedImages);
@@ -41,41 +40,27 @@ function App() {
    return (
      <div className='form'>
        <form action="/upload" accept="image/*" method="post" encType="multipart/form-data">
-     <div>
-       <form action="/upload" method="post" encType="multipart/form-data">
-         <div>
-           <label>Select your cat pictures:</label>
-           <input 
-             multiple 
-             name="cat_pics" 
-             type="file"
-             accept=".jpg,.jpeg,.png,.gif,.tiff,.psd,.pdf,application/postscript,image/bmp,image/heif,image/svg+xml"
-             onChange={(e) => setFile(e.target.files)} />
-           <h1 className=''>Select your cat pictures:</h1>
-           <input multiple="multiple" name="cat_pics" type="file"
-                  onChange={(e) => setFile(e.target.files)} />
-           {}
-           <button type="button" onClick={upload}>Upload Cat Pics</button>
-         </div>
+         <label>Select your cat pictures:</label>
+         <input
+           multiple
+           name="cat_pics"
+           type="file"
+           accept=".jpg,.jpeg,.png,.gif,.tiff,.psd,.pdf,application/postscript,image/bmp,image/heif,image/svg+xml"
+           onChange={(e) => setFile(e.target.files)} />
+         {}
+         <button type="button" onClick={upload}>Upload Cat Pics</button>
        </form>
-
+       
        {imageUrls.map((url, index) =>
          (
-            <div key={index}>
-              <ImageCard imageUrl={`http://localhost:4000${url}`} />
-              {/* Add a delete button with an onClick event */}
-              <button onClick={() => handleDelete(index)}>Delete</button> 
-            </div>  
-         )
+           <div key={index}>
+             <ImageCard imageUrl={`http://localhost:4000${url}`} />
+             {/* Add a delete button with an onClick event */}
+             <button onClick={() => handleDelete(index)}>Delete</button>
+            </div>
+          )
        )}
      </div> 
-      <div className='images'>
-      {imageUrls.map((url, index) =>
-              <ImageCard key={index} imageUrl={`http://localhost:4000${url}`} />
-            )}
-      </div>
-       
-     </div>
    );
 }
 
