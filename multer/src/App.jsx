6146a2bd8e036670c09f9f18bbeaaa576f33b966
jnepmark <123545579+jnepmark@users.noreply.github.com>
2024-04-22 20:14:04 +0200

    eege

--- multer/src/App.jsx
+++ multer/src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import ImageCard from './ImageCard';
 
 function App() {
-  const[file, setFile] = useState();
+  const [file, setFile] = useState();
   const [imageUrls, setImageUrls] = useState([]);
 
   const upload = () => {
@@ -30,22 +30,39 @@ function App() {
       .catch(err => console.log(err));
   }
 
+   const handleDelete = (index) => {
+     // Create a copy of the imageUrls array without the deleted item
+     let updatedImages = [...imageUrls];
+     updatedImages.splice(index, 1);
+     setImageUrls(updatedImages);
+   }
+
    return (
      <div>
-       <form action="/upload" accept="image/*" method="post" encType="multipart/form-data">
+       <form action="/upload" method="post" encType="multipart/form-data">
          <div>
            <label>Select your cat pictures:</label>
-           <input multiple="multiple" name="cat_pics" type="file"
-                  onChange={(e) => setFile(e.target.files)} />
+           <input 
+             multiple 
+             name="cat_pics" 
+             type="file"
+             accept=".jpg,.jpeg,.png,.gif,.tiff,.psd,.pdf,application/postscript,image/bmp,image/heif,image/svg+xml"
+             onChange={(e) => setFile(e.target.files)} />
            {}
            <button type="button" onClick={upload}>Upload Cat Pics</button>
          </div>
        </form>
 
        {imageUrls.map((url, index) =>
-         <ImageCard key={index} imageUrl={`http://localhost:4000${url}`} />
+         (
+            <div key={index}>
+              <ImageCard imageUrl={`http://localhost:4000${url}`} />
+              {/* Add a delete button with an onClick event */}
+              <button onClick={() => handleDelete(index)}>Delete</button> 
+            </div>  
+         )
        )}
-     </div>
+     </div> 
    );
 }
 
